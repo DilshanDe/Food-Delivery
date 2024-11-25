ffcc46e546cc6a3230890dc514ae3daa892ebdb8
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-11-25 21:22:48 +0530

    Update App.js

--- client/App.js
+++ client/App.js
@@ -1,11 +1,19 @@
 // App.js
 import React from 'react';
 import AppNavigator from './AppNavigator';
+import { Provider } from 'react-redux'
+import { store } from './store';
 
 
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <Provider store={store}>
+       <AppNavigator />
+
+    </Provider>
+ 
+  );
 }
 
 
